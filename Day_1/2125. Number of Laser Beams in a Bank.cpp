894ecbb18f8518a764e93fbebe7d218ecc2e962e
Syed Najam U Saqib <136888817+syednajamusaqib@users.noreply.github.com>
2024-01-03 21:01:10 +0500

    Update 2125. Number of Laser Beams in a Bank.cpp

--- Day_1/2125. Number of Laser Beams in a Bank.cpp
+++ Day_1/2125. Number of Laser Beams in a Bank.cpp
@@ -1,1 +1,23 @@
 
+class Solution {
+public:
+    int numberOfBeams(vector<string>& bank) {
+        int n = bank.size();
+        int prevRowDevices = 0;
+        int ans = 0;
+
+        for(int i = 0; i < n; i++){
+            int currRowDevices = 0;
+
+            for(char ch: bank[i]){
+                if(ch == '1') currRowDevices++;
+            }
+
+            ans += prevRowDevices * currRowDevices;
+            if(currRowDevices != 0){
+                prevRowDevices = currRowDevices;
+            }
+        }
+        return ans;
+    }
+};
