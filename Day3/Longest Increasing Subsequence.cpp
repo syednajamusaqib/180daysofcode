da5733a823990eccd872ec38b179992f7e02b0b7
Syed Najam U Saqib <136888817+syednajamusaqib@users.noreply.github.com>
2024-01-05 21:23:02 +0500

    Update Longest Increasing Subsequence.cpp

--- Day3/Longest Increasing Subsequence.cpp
+++ Day3/Longest Increasing Subsequence.cpp
@@ -1,1 +1,35 @@
 
+class Solution {
+public:
+    int n;
+    // Memoization
+    int dp[2501][2501]; // Depends on Constraints
+    int solve(vector<int>& nums, int index, int prev_index){
+        // Base Case
+        if(index >= n){
+            return 0;
+        }
+
+        if(prev_index != -1 && dp[index][prev_index] != -1){
+            return dp[index][prev_index];
+        }
+
+        // Condition to check whether we can take sequence or not
+        int take = 0;
+        if(prev_index == -1 || nums[index] > nums[prev_index]){
+            take = 1 + solve(nums, index + 1, index);
+        }
+        // Condition to check whether we skip sequence or not
+        int skip = solve(nums,index + 1,prev_index);
+        if(prev_index != -1){
+            dp[index][prev_index] = max(take,skip);
+        }
+        return max(skip,take);
+    }
+    int lengthOfLIS(vector<int>& nums) {
+        n = nums.size();
+        // Initializing with -1
+        memset(dp,-1,sizeof(dp));
+        return solve(nums, 0, -1);
+    }
+};
