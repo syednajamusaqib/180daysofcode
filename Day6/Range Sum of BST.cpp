937eca4cb7779e487355eda49e277d13fb4177e1
Syed Najam U Saqib <136888817+syednajamusaqib@users.noreply.github.com>
2024-01-08 20:03:52 +0500

    Create Range Sum of BST.cpp

---
+++ Day6/Range Sum of BST.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int rangeSumBST(TreeNode* root, int low, int high) {
+        if(root == NULL){
+            return 0;
+        }
+
+        if(root->val >= low && root->val <= high){
+            return root->val + rangeSumBST(root->right,low,high) 
+                             + rangeSumBST(root->left,low,high);
+        }
+
+        if(root-> val < low){
+            return rangeSumBST(root->right,low,high);
+        }
+
+            return rangeSumBST(root->left,low,high);
+
+    }
+};
