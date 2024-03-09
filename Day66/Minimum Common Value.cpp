7821b81cfa05ee626ce4f905a0ea3185006c0e21
Syed Najam U Saqib <136888817+syednajamusaqib@users.noreply.github.com>
2024-03-09 09:54:47 +0500

    Create Minimum Common Value.cpp

---
+++ Day66/Minimum Common Value.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int getCommon(vector<int>& nums1, vector<int>& nums2) {
+        int i = 0;
+        int j = 0;
+        int ans = -1;
+
+        while(i < nums1.size() && j < nums2.size()){
+            if(nums1[i] == nums2[j]){
+                ans = nums1[i];
+                break;
+            }else if(nums1[i] < nums2[j]){
+                i++;
+            }else{
+                j++;
+            }
+        }
+        return ans;
+    }
+};
